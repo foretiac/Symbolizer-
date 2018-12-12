ed029e7d2f307de26811d69ef34fa5ca2bc68924
foretiac <44907739+foretiac@users.noreply.github.com>
2018-12-12 01:34:56 -0500

    Add files via upload

---
+++ 340 Symbolizer.py
@@ -0,0 +1,30 @@
+#Anthony Foretich
+#ISAT 340
+#Symbolizer program
+
+# create constants
+variable = 0
+
+#Create a Tuple 
+Tuple = ('Blue', 'Red', 8, 9.6)
+print (Tuple)
+
+
+#Create an Array 
+Array = ['Blue', 'Black', 7, 8.7]
+print (Array)
+
+#Create a set
+
+Set = {1, 'ten', 6.0}
+print (Set)
+
+# create variable and iterating function
+
+for i in range (3):
+    variable += 1 
+    print (variable)
+
+
+
+
